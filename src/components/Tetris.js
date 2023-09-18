7d3cc61470f4ed7c51a1e9fd53b9d106e158af73
Lauren Keenan <19015831@uhi.ac.uk>
2023-09-18 15:09:36 +0100

    create and import styled-components

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,30 +1,36 @@
 import React from 'react';
 
 import { createStage } from '../gameHelpers';
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 // components
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 
+
 const Tetris = () => {
 
 
     return (
-        <div>
-            {/* will take in props eventually */}
-            <Stage stage={createStage()} />
-
-            {/* this is the side display stack */}
-            <aside>
+        <StyledTetrisWrapper>
+            <StyledTetris>
                 <div>
-                    <Display text="Score" />
-                    <Display text="Rows" />
-                    <Display text="Level" />
+                    {/* will take in props eventually */}
+                    <Stage stage={createStage()} />
+
+                    {/* this is the side display stack */}
+                    <aside>
+                        <div>
+                            <Display text="Score" />
+                            <Display text="Rows" />
+                            <Display text="Level" />
+                        </div>
+                        <StartButton />
+                    </aside>
                 </div>
-                <StartButton />
-            </aside>
-        </div>
+            </StyledTetris>
+        </StyledTetrisWrapper>
     );
 };
 
