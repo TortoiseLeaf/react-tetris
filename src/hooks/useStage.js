490f5018c14c8e56463b7cdc06db54f316fa17ca
Lauren Keenan <19015831@uhi.ac.uk>
2023-09-27 20:26:05 +0100

    clear rows

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,22 +1,35 @@
 import { useState, useEffect } from 'react';
 import { createStage } from '../gameHelpers';
-import { usePlayer } from './usePlayer';
-
 
 
 export const useStage = (player, resetPlayer) => {
-    console.log("new log: ", resetPlayer)
-    const [stage, setStage] = useState(createStage())
+    const [stage, setStage] = useState(createStage());
+    const [rowsCleared, setRowsCleared] = useState(0);
 
 
     useEffect(() => {
+        setRowsCleared(0);
+
+        const sweepRows = newStage =>
+        // ack is new array built up inside reduce function
+            newStage.reduce((ack, row) => {
+                // if no 0 cell is found in row don't clear it
+                // if it doesn't find 0 it will return -1 and we know to clear the row
+                if (row.findIndex(cell => cell[0] === 0) === -1) {
+                    setRowsCleared(prev => prev + 1);
+                    ack.unshift(new Array(newStage[0].length).fill([0,'clear']));
+                    return ack;
+                }
+                ack.push(row);
+                    return ack;
+            }, [])
+
         const updateStage = prevStage => {
             // first flush the stage
             const newStage = prevStage.map(row => 
                 row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell))
                 );
 
-                console.log("newStage before collided", newStage)
             // draw the tetromino
             player.tetromino.forEach((row, y) => {
                 row.forEach((value, x) => {
@@ -28,20 +41,18 @@ export const useStage = (player, resetPlayer) => {
                     }
                 });
             })
-            console.log("newstage after collided", newStage)
 
             // check if collided
-            console.log("resetPlayer is", resetPlayer)
             if (player.collided) {
                 resetPlayer();
+                return sweepRows(newStage);
             }
 
             return newStage;
         };
 
         setStage(prev => updateStage(prev))
-
     }, [player, resetPlayer]);
 
-    return [stage, setStage];
+    return [stage, setStage, rowsCleared];
 };
